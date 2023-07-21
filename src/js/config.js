cd1a3891589285132575d191c441c1ab3545c83c
Fathy-Elgazar <fathyelgazar348@gmail.com>
2023-07-21 04:04:47 +0300

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = "c8cff8ea-ee25-4617-84bf-82acb8bbb946";
+export const MODAL_SEC_CLOSE = 2.5;
